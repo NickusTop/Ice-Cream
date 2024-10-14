10a3a49e54d79708c8d91331ebfc00d148091e9d
NickusTop <148118087+NickusTop@users.noreply.github.com>
2024-10-14 17:56:33 +0000

    Deploying to gh-pages from @ NickusTop/Ice-Cream@117193b2101fd94aa50bf79867fc787c5f74d84e 🚀

---
+++ index.a7aea3ef.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",(function(){var e=document.body,n=document.getElementById("menu"),t=document.getElementById("openMenu"),c=document.getElementById("closeMenu");t.addEventListener("click",(function(){n.classList.add("active"),e.classList.add("no-scroll")})),c.addEventListener("click",(function(){n.classList.remove("active"),e.classList.remove("no-scroll")}))}));
+//# sourceMappingURL=index.a7aea3ef.js.map
--- index.fd1a1e7c.js
+++
@@ -1,2 +0,0 @@
-var menu=document.querySelector(".menu"),menuToggle=document.getElementById("menuToggle");menuToggle.addEventListener("click",(function(){"-250px"===menu.style.left?menu.style.left="0":menu.style.left="-250px"}));
-//# sourceMappingURL=index.fd1a1e7c.js.map
