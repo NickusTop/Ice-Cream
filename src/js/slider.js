6ddcfe8e5a91e810152a7137c6b36346652a5e2f
NickusTop <148118087+NickusTop@users.noreply.github.com>
2024-10-28 18:02:47 +0200

    End

---
+++ src/js/slider.js
@@ -0,0 +1,25 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
