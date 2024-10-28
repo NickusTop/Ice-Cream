63da3f9215e8b771ff6c0ea3e962ffcc342e1b96
NickusTop <148118087+NickusTop@users.noreply.github.com>
2024-10-28 16:05:22 +0000

    Deploying to gh-pages from @ NickusTop/Ice-Cream@43e6b7da332bcaf1508edf3e8c79a5704000a4f1 🚀

--- index.ad4bef7f.js
+++
@@ -1,2 +0,0 @@
-var swiper=new Swiper(".swiper",{loop:!0,pagination:{el:".swiper-pagination",clickable:!0},keyboard:{enabled:!0,onlyInViewport:!1},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},scrollbar:{el:".swiper-scrollbar"}});
-//# sourceMappingURL=index.ad4bef7f.js.map
